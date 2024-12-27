f91f78b88636113b4211c1e736740704f3c23aea
Jonatan <jonatan.e.vera@gmail.com>
2024-12-27 07:15:07 -0300

    implementacion de discovery server (eureka server) y registro de servicios

---
+++ discovery-server/src/test/java/com/msvc/discovery/server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msvc.discovery.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
